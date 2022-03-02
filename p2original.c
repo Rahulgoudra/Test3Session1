f85d9304b726e81c33affd83e80310b37c957610
Rahul G <97090660+Rahulgoudra@users.noreply.github.com>
2022-03-02 18:26:46 +0000

    done

---
+++ p2original.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+  int a,b,multiply;
+  printf("enter a two numbers");
+  scanf("%d%d",&a,&b);
+  multiply=a*b;
+  printf("the multiplication is %d",multiply);
+  return 0;
+}
