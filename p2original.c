44510ae1b7eca7ed9653f8209db2c5c86e6b91c5
Rahul G <97090660+Rahulgoudra@users.noreply.github.com>
2022-03-03 16:05:48 +0000

    done

--- p2original.c
+++ p2original.c
@@ -1,10 +1,48 @@
 #include<stdio.h>
 int main()
 {
-  int a,b,multiply;
-  printf("enter a two numbers");
-  scanf("%d%d",&a,&b);
-  multiply=a*b;
-  printf("the multiplication is %d",multiply);
+  int num,den;
+  printf("enter two numbers/n",&num,&den);
+  int a= num1,den1;
+  printf("enter two numbers/n",&num1,&den1);
+  scanf("%d,%d"&num1,&den1);
+    int b=num2,den2;
+  printf("enter two numbers/n",&num2,&den2);
+  scanf("%d,%d"&num2,&den2);
+    int c=num3,den3;
+  printf("enter two numbers/n",&num1,&den1);
+  scanf("%d,%d"&num3,&den3);
   return 0;
 }
+Fraction Largest_fraction('Fractionf1, Fraction f2, Fraction f3');
+'{'
+  int largest_fraction("Fraction f1, Fraction f2, Fraction f3");
+  {
+    if(f1>f2)
+    {
+      if(f1>f3)
+      {
+        return f1;
+      }
+      else
+      {
+        return f3;
+      }
+    }
+    else
+    {
+      if(f2>f3)
+      {
+        return f2;
+      }
+      else
+      {
+        return f3;
+      }
+    }
+  }
+void output(Fraction f1, Fraction f2, Fraction f3, Fraction largest)
+  {
+    printf("the largest fraction is %d%d\n"largest);
+  }
+
